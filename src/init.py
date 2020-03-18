ce25c2874ebc680e09e8474074044bab8b5c8574
Shimizu Yasushi <decode4423@gmail.com>
2020-03-18 22:45:46 +0900

    python一通り、動作確認中

--- src/init.py
+++ src/init.py
@@ -13,7 +13,8 @@ TAPEs = _variables.TAPEs
 LEDs = _variables.LEDs
 width,height=_variables.width, _variables.height
 camera = _variables.camera
-port_num = _variables.port_num
+send_port = _variables.send_port
+rcv_port = _variables.rcv_port
 ip = _variables.ip
 once = 128
 count_once = int(math.log(once, 2))
@@ -23,47 +24,54 @@ for LED in LEDs:
 	count_init.append(LED//once)
 	c += 1
 
-host = "192.168.1.9"
-port = 37564
 for i in ip:
-	print("HOST:" + i +", PORT:" + str(port))
+	print("HOST:" + i +", PORT:" + str(send_port))
 
 print(TAPEs)
 print(LEDs)
 print(str(width) + "x" + str(height))
+send_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+rcv_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+rcv_client.bind(("192.168.0.124", rcv_port))
 
 offset = 0
 pixels_bin = [[[] for col in range(width)] for row in range(height)]
-pixels = [[-1 for col in range(width)] for row in range(height)]
+pixels = [[[-1, -1] for col in range(width)] for row in range(height)]
 colors = [[0 for col in range(width)] for row in range(height)]
 frames = []
 
 def send_soc(ip, items):
-	socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	socket_client.connect((ip, port))
 	if(type(items) is list):
 		msg = json.dumps(items)
 	elif(type(items) is str):
 		msg = items
-	socket_client.send(msg.encode('utf-8'))
-	response = socket_client.recv(4096)
-	while not response:
-		print('Wait for response.....')
+	send_client.sendto(msg.encode('utf-8'), (ip, send_port))
+	while True:
+		try:
+			rcv_byte = bytes()
+			rcv_byte, addr = rcv_client.recvfrom(64)
+			msg = rcv_byte.decode()
+			print(msg)
+			if msg == '1':
+				break
+		except KeyboardInterrupt:
+				rcv_client.close()
 
 def onLed():
 	global offset, frames
-	for c in range(len(count_init)):
-		send_soc(ip[c], "off")
-		for i in range(count_init[c]):
+	cap = cv2.VideoCapture(camera)
+	for cnt_esp in range(len(count_init)):
+		send_soc(ip[cnt_esp], "off")
+		for i in range(count_init[cnt_esp]):
 			onLed = []
-			for cnt in range (2):
+			for cnt_rvrc in range (2):
 				for j in range(count_once):
 					onLed_oneSctn = int(once / (2 ** (j + 1)))
 					onled_sep = 2 ** (j + 1)
 					onLed = []
 					for sep_pos in range(onled_sep):
 						for k in range(offset, onLed_oneSctn + offset):
-							if(cnt == 0):
+							if(cnt_rvrc == 0):
 								if(sep_pos%2 == 0):
 									onLed.append(1)
 								else:
@@ -74,16 +82,16 @@ def onLed():
 								else:
 									onLed.append(1)
 					onLed = [i + offset for i, x in enumerate(onLed) if x == 1]
-					send_soc(ip[c], onLed)
-					analyze(capture())
-					send_soc(ip[c], "off")
+					send_soc(ip[cnt_esp], onLed)
+					analyze(capture(cap))
+					send_soc(ip[cnt_esp], "off")
+					#sleep(0.1)
 				# for ana_item in frames:
 				# 	analyze(ana_item)
-				apply(cnt)
+				apply(cnt_rvrc, cnt_esp)
 			offset += once
 
-def capture():
-	cap = cv2.VideoCapture(camera)
+def capture(cap):
 	ret, frame = cap.read()
 	frame = cv2.resize(frame,(width, height))
 	return frame
@@ -104,7 +112,7 @@ def analyze(frame):
 			else:
 				pixels_bin[i][j].append(0)
 
-def apply(count):
+def apply(count_reverce, count_esp):
 	global pixels_bin, pixels, colors, offset
 	for j in range(width):
 		for i in range(height):
@@ -113,24 +121,29 @@ def apply(count):
 				for num in pixels_bin[i][j]:
 					bin += str(num)
 				bin = int(bin, 2)
-				if(count == 0):
+				if(count_reverce == 0):
 					ledNum = once - bin + offset
 				else:
 					ledNum = bin + offset
 				column = 0
 				flag_lednum = False
 				for row in range(height):
-					if(pixels[row].count(ledNum) > 0):
-						column = pixels[row].index(ledNum)
-						flag_lednum = True
-						break
+					for i in range(len(ip)):
+						if(pixels[row][i][0]==ledNum):
+							flag_lednum = True
+							break
+					else:
+						continue
+					break
 				if(not flag_lednum):
-					pixels[i][j] = ledNum
+					pixels[i][j][0] = ledNum
+					pixels[i][j][1] = count_esp
 				else:
-					same_c = colors[row][column]
+					same_c = colors[row][i]
 					if(same_c < colors[i][j]):
-						same = pixels[row][column]
-						pixels[i][j] = ledNum
+						same = pixels[row][i]
+						pixels[i][j][0] = ledNum
+						pixels[i][j][1] = count_esp
 						pixels[row][column] = -1
 
 
---
+++ src/video.py
@@ -0,0 +1,67 @@
+import cv2
+import numpy as np
+import socket
+from time import sleep
+import json
+import _variables
+
+TAPEs = _variables.TAPEs
+LEDs = _variables.LEDs
+width,height=_variables.width, _variables.height
+camera = _variables.camera
+send_port = _variables.send_port
+ip = _variables.ip
+
+json_open = open('pos.json', 'r')
+pos = json.load(json_open)
+send_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+def send_soc(ip, items):
+	if(type(items) is list):
+		msg = json.dumps(items)
+	elif(type(items) is str):
+		msg = items
+	print(msg)
+	send_client.sendto(msg.encode('utf-8'), (ip, send_port))
+
+def video_send():
+	video = cv2.VideoCapture("./video/bad.mp4")
+	while(True):
+		colors = [[] for i in range(len(ip))]
+		ret, videoFrame = video.read()
+		if not ret:
+			video.set(cv2.CAP_PROP_POS_FRAMES, 0)
+			continue
+		videoFrame = cv2.resize(videoFrame,(width, height))
+		cv2.imshow("frame", videoFrame)
+		for j in range(width):
+			for i in range(height):
+				if(pos[i][j][0] > 0 and 2700 > pos[i][j][0]):
+					pixVal = videoFrame[i, j]
+					g = hex(pixVal[1]).strip("0x")
+					r = hex(pixVal[2]).strip("0x")
+					b = hex(pixVal[0]).strip("0x")
+					if(len(g) == 1):
+						g = "0" + g
+					elif(len(g) == 0):
+						g = "00"
+					if(len(r) == 1):
+						r = "0" + r
+					elif(len(r) == 0):
+						r = "00"
+					if(len(b) == 1):
+						b = "0" + b
+					elif(len(b) == 0):
+						b = "00"
+					color = "0x" + g + r + b
+					colors[pos[i][j][1]].append(pos[i][j][0])
+					colors[pos[i][j][1]].append(color)
+		for k in range(len(ip)):
+			send_soc(ip[k], colors[k])
+		k = cv2.waitKey(1)
+		if k == 27:
+			break
+	video.release()
+	cv2.destroyAllWindows()
+
+video_send()
