fed3e9d051da04dd2f0915af799f159ae1a67183
Shimizu Yasushi <decode4423@gmail.com>
2020-03-21 14:20:36 +0900

    及第点

--- src/rag_esp32/rag_esp32.ino
+++ src/rag_esp32/rag_esp32.ino
@@ -3,31 +3,38 @@
 #include <FastLED.h>
 #include <WiFi.h>
 #include <WiFiUdp.h>
+#include <iostream>
+#include <string>
 // const char* SSID = "Buffalo-G-6110";
 // const char* PASSWD = "17709934";
 // const char* SSID = "SIAF-FREE-WiFi";
 // const char* PASSWD = "siafsiaf";
-const char* SSID = "kouheki";
-const char* PASSWD = "kouheki0000";
-WiFiServer server(37564);
-
 #define NUM_LED 600
 #define DATA_PIN 0
 CRGB leds[NUM_LED];
 
+const char* SSID = "kouheki";
+const char* PASSWD = "kouheki0000";
+#define port 10001
+int to_port = 11001;
+IPAddress ip(192, 168, 0, 121);
+
+
+IPAddress gateway(192,168, 0, 1);
+IPAddress subnet(255, 255, 255, 0);
+IPAddress DNS(192, 168, 0, 90); 
+char *to_ip = "192.168.0.120";
+
 WiFiUDP udp;
-
-char *to_ip = "192.168.0.124";
-int to_port = 18782;
-
+WiFiServer server(port);
 void setup(){
   Serial.begin(115200);
   FastLED.addLeds<WS2812B, DATA_PIN>(leds, NUM_LED);
-  FastLED.setBrightness(255);
+  FastLED.setBrightness(128);
   allOff();
   wifi_connect(SSID, PASSWD);
   WiFi.mode(WIFI_STA);
-  udp.begin(37564);
+  udp.begin(port);
   Serial.println("Server started");
 }
 
@@ -37,21 +44,34 @@ void loop() {
   int packetSize = udp.parsePacket();
   if(packetSize){
     int len = udp.read(packetBuffer, packetSize);
-    if (len > 0) packetBuffer[len] = '\0';
+    if(len > 0){
+      packetBuffer[len] = '\0';
+    }
     String msg = packetBuffer;
     if(msg.startsWith("/video/")){
-      msg = msg.replace("/video/", "");
+      msg.replace("/video/", "");
       rcv_video(msg);
     }
     else if(msg.startsWith("/init/")){
-      msg = msg.replace("/init/", "");
+      msg.replace("/init/", "");
+      Serial.println(msg);
       rcv_init(msg);
       send_vrf();
     }
   }
 }
 
+void prnt(String str = "", int i = 0){
+  if(str != ""){
+    Serial.println(str);
+  }
+  if(i != 0){
+    Serial.println(i);
+  }
+}
+
 void wifi_connect(const char* ssid, const char* password){
+  WiFi.config(ip, gateway, subnet, DNS);
   WiFi.begin(ssid, password);
   while(WiFi.status() != WL_CONNECTED){
     Serial.println(".");
@@ -75,7 +95,7 @@ void allOff(){
   FastLED.show();
 }
 
-void rcv_init(msg){
+void rcv_init(String msg){
   if(msg == "off"){
     allOff();
   }
@@ -85,7 +105,7 @@ void rcv_init(msg){
   FastLED.show();
 }
 
-void rcv_video(msg){
+void rcv_video(String msg){
   if(msg == "off"){
     allOff();
   }
@@ -99,28 +119,23 @@ void on_init(String msg){
   const size_t capacity = JSON_ARRAY_SIZE(1024);
   DynamicJsonDocument doc(capacity);
   deserializeJson(doc, msg);
-  for(int i = 0; i < 64; i++){
+  Serial.println(measureMsgPack(doc));
+  for(int i = 0; i < doc.size(); i++){
     int num = doc[i];
-    if(i == sizeof(doc) - 1){
-      Serial.println(num);
-    }
-    else{
-      Serial.print(num);
-      Serial.print(",");
-    }
-    leds[num] = 0xffffff;
+    leds[num] = 0x0e0e0e;
   }
 }
 
-//measureMsgPack(doc)のチェック
-//on_init()でやると良い
-void on_video(){
+void on_video(String msg){
   const size_t capacity = JSON_ARRAY_SIZE(1125);
   DynamicJsonDocument doc(capacity);
   deserializeJson(doc, msg);
-  for(int i = 0; i < measureMsgPack(doc); i += 2){
+  for(int i = 0; i < doc.size(); i += 2){
     int num = doc[i];
-    int color = doc[i+1]
-    leds[num] = color;
+    int r = doc[i+1][0];
+    int g = doc[i+1][1];
+    int b = doc[i+1][2];
+    Serial.print("R: ");Serial.print(r);Serial.print(" G: "); Serial.print(g);Serial.print(" B: "); Serial.println(b);
+    leds[num] = CRGB(r, g, b);
   }
 }
--- src/video.py
+++ src/video.py
@@ -10,22 +10,21 @@ LEDs = _variables.LEDs
 width,height=_variables.width, _variables.height
 camera = _variables.camera
 send_port = _variables.send_port
-ip = _variables.ip
+ip = _variables.send_ip
 
 json_open = open('pos.json', 'r')
 pos = json.load(json_open)
 send_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-def send_soc(ip, items):
+def send_soc(ip, port, items):
 	if(type(items) is list):
 		msg = "/video/" + json.dumps(items)
 	elif(type(items) is str):
 		msg = items
-	print(msg)
-	send_client.sendto(msg.encode('utf-8'), (ip, send_port))
+	send_client.sendto(msg.encode('utf-8'), (ip, port))
 
 def video_send():
-	video = cv2.VideoCapture("./video/bad.mp4")
+	video = cv2.VideoCapture("./video/test.mp4")
 	while(True):
 		ret, videoFrame = video.read()
 		if not ret:
@@ -38,27 +37,16 @@ def video_send():
 			for i in range(height):
 				if(pos[i][j][0] > 0 and 2700 > pos[i][j][0]):
 					pixVal = videoFrame[i, j]
-					g = hex(pixVal[1]).strip("0x")
-					r = hex(pixVal[2]).strip("0x")
-					b = hex(pixVal[0]).strip("0x")
-					if(len(g) == 1):
-						g = "0" + g
-					elif(len(g) == 0):
-						g = "00"
-					if(len(r) == 1):
-						r = "0" + r
-					elif(len(r) == 0):
-						r = "00"
-					if(len(b) == 1):
-						b = "0" + b
-					elif(len(b) == 0):
-						b = "00"
-					color = "0x" + g + r + b
+					r = videoFrame.item(i, j, 1)
+					g = videoFrame.item(i, j, 2)
+					b = videoFrame.item(i, j, 0)
+					color = [r, g, b]
 					colors[pos[i][j][1]].append(pos[i][j][0])
 					colors[pos[i][j][1]].append(color)
 			for k in range(len(ip)):
-				send_soc(ip[k], colors[k])
-			#sleep(0.1)
+				if(len(colors[k]) > 0):
+					send_soc(ip[k], send_port[k], colors[k])
+			sleep(0.0001)
 		k = cv2.waitKey(1)
 		if k == 27:
 			break
